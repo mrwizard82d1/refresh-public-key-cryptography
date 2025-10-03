7304714e9630c523e1bd42bb4beeb94a5f802a29
Larry Jones <mrwizard82d1@gmail.com>
2025-10-02 19:29:56 -0500

    Add API /items/{item_id}

--- verifier_web_service/main.py
+++ verifier_web_service/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str) -> dict[str, str]:
+    return {"item_id": item_id}
